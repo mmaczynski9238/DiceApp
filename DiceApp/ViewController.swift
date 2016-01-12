b228f3ddbb798218480c1309644cef36166685c2
Michael Maczynski <mmaczynski@jh-mbair-158-17.d214.org>
2016-01-12 14:45:22 -0600

    Started playing with Shuffle

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -10,11 +10,63 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        
     }
 
+    
+    let myArray = [1,2,3,4,5,6]
+    let tempArray = [1,2,3,4,5,6]
+
+
+    
+    /*func shuffleArray<T>(array: Array<T>) -> Array<T>
+    {
+        for var index = array.count - 1; index > 0; index--
+        {
+            // Random int from 0 to index-1
+            var j = Int(arc4random_uniform(UInt32(index-1)))
+            
+            // Swap two array elements
+            // Notice '&' required as swap uses 'inout' parameters
+            swap(&array[index], &array[j])
+        }
+        return array
+    }
+
+    */
+    
+    func shuffleArray(array: [Int]) -> [Int] {
+        
+        var tempArray = array
+        for index in 0...array.count - 1 {
+            let randomNumber = arc4random_uniform(UInt32(myArray.count - 1))
+            let randomIndex = Int(randomNumber)
+            tempArray[randomIndex] = array[index]
+        }
+        
+        return tempArray
+    }
+    
+    
+    shuffleArray(myArray)
+    //shuffleArray(myArray)
+    //shuffleArray(myArray)
+
+    
+    
+    
+    
+    
+    @IBAction func RollButton(sender: UIButton) {
+        
+        print("\tempArray")
+        print("myArray")
+
+        
+    }
 
 }
-
