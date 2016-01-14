a1ba67f78a79cb44ea14cd3e8a88c3ed32c0f347
Michael Maczynski <mmaczynski@jh-mbair-158-17.d214.org>
2016-01-14 14:49:39 -0600

    Finished Randomize Function

--- DiceApp/ViewController.swift
+++ DiceApp/ViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var DiceLabel: UILabel!
     
     
     override func viewDidLoad() {
@@ -18,53 +19,20 @@ class ViewController: UIViewController {
     }
 
     
-    let myArray = [1,2,3,4,5,6]
-    let tempArray = [1,2,3,4,5,6]
-
-
-    
-    /*func shuffleArray<T>(array: Array<T>) -> Array<T>
-    {
-        for var index = array.count - 1; index > 0; index--
-        {
-            // Random int from 0 to index-1
-            var j = Int(arc4random_uniform(UInt32(index-1)))
-            
-            // Swap two array elements
-            // Notice '&' required as swap uses 'inout' parameters
-            swap(&array[index], &array[j])
-        }
-        return array
-    }
-
-    */
-    
-    func shuffleArray(array: [Int]) -> [Int] {
-        
-        var tempArray = array
-        for index in 0...array.count - 1 {
-            let randomNumber = arc4random_uniform(UInt32(myArray.count - 1))
-            let randomIndex = Int(randomNumber)
-            tempArray[randomIndex] = array[index]
-        }
-        
-        return tempArray
-    }
-    
-    
-    shuffleArray(myArray)
-    //shuffleArray(myArray)
-    //shuffleArray(myArray)
-
-    
+    var numbers = [5,1,6,2,4,3,1,3,2,6,4,5]
     
     
     
     
     @IBAction func RollButton(sender: UIButton) {
         
-        print("\tempArray")
-        print("myArray")
+        
+        
+        let randomIndex = Int(arc4random_uniform(UInt32(numbers.count)))
+        let randomNumber = numbers[randomIndex]
+
+        
+        DiceLabel.text = "\(randomNumber)"
 
         
     }
